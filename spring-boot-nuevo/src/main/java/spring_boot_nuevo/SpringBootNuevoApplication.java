73a7b1ee8ecd1845ab34f5fa4d1fb279852e5acd
pnferrey <pnferrey@gmail.com>
2024-11-26 18:03:07 +0100

    Add files via upload

---
+++ spring-boot-nuevo/src/main/java/spring_boot_nuevo/SpringBootNuevoApplication.java
@@ -0,0 +1,13 @@
+package spring_boot_nuevo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNuevoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootNuevoApplication.class, args);
+	}
+
+}
---
+++ spring-boot-nuevo/src/test/java/spring_boot_nuevo/SpringBootNuevoApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_boot_nuevo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootNuevoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
