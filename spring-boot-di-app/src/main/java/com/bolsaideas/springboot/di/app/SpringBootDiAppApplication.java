9fee01d7baee9799554592eeff20772db31137d1
pnferrey <pnferrey@gmail.com>
2024-11-26 18:01:01 +0100

    Add files via upload

---
+++ spring-boot-di-app/src/main/java/com/bolsaideas/springboot/di/app/SpringBootDiAppApplication.java
@@ -0,0 +1,13 @@
+package com.bolsaideas.springboot.di.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDiAppApplication.class, args);
+	}
+
+}
